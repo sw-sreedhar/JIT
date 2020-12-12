a304ebc2bb1847f1cde71e9ff2d6a87defca9b12
Sreedhar Reddy Nallavelly <sreedharreddynallavelly@Sreedhars-MacBook-Pro.local>
2020-12-12 11:26:35 -0800

    latest

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++){
-            System.out.println("Hello Modified file to check Jenkins Job run: "+i);
+            System.out.println("hello man How are you: "+i);
         }
     }
 }
