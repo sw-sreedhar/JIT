3e86eea54fdca26051ea395a9968200c5fb26589
Sreedhar Reddy Nallavelly <sreedharreddynallavelly@Sreedhars-MacBook-Pro.local>
2020-12-12 11:48:27 -0800

    hello

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
 
         for (int i = 0; i < 10; i++){
-            System.out.println("hello man How are you: "+i);
+            System.out.println("making a change for the git push: "+i);
         }
     }
 }
